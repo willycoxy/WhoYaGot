7df0d21fbecbe0eb8cd9a8e65f2fbebe4bf207e4
willycoxy <williamjwcox@gmail.com>
2022-10-31 20:16:40 -0400

    fixing commit issues

---
+++ client/src/App.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Navbar, Nav, Container, Modal, Tab } from 'react-bootstrap';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import Header from './components/Header';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import Home from './pages/Home';
+import NoMatch from './pages/NoMatch';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+function App() {
+
+  return (
+    
+      <ApolloProvider client={client}>
+        <Router> 
+          <Header />
+            <div className = "container" >
+              <Routes>
+                <Route path="/" exact element={<Home></Home>}></Route>
+                <Route path="/signup" element={<Signup></Signup>}></Route>
+                <Route path="/login" element={<Login></Login>}></Route>
+                <Route path="*" element={<NoMatch />} 
+            />
+              </Routes>
+            </div>
+        </Router>
+      </ApolloProvider> 
+  );
+}
+
+export default App;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,44 @@
+// import the gql tagged template function
+const { gql } = require('apollo-server-express');
+
+// create our typeDefs
+const typeDefs = gql`
+
+type Player{
+    _id: ID
+    TeamID: Int
+    PlayerID: Int
+    Name: String
+    Team: String
+    Position: String
+    FantasyPoints: Float
+  }
+
+  type User {
+    _id: ID
+    username: String
+    email: String
+    players: [Player]
+  }
+
+  type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+    players: [Player]
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+`;
+
+// export the typeDefs
+module.exports = typeDefs;
