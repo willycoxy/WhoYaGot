ece22e292b7659ab5dd84ff9c7199b670cb273fd
MiguelJerome <sharkmaycry@gmail.com>
2022-11-02 15:03:54 -0400

    clean component team codes

--- client/src/components/Coyotes/index.js
+++ client/src/components/Coyotes/index.js
@@ -1,51 +1,12 @@
-import React from 'react';
-import '../components/Player/player.css';
-import { Container } from 'react-bootstrap';
-import { useQuery } from '@apollo/client';
-import { QUERY_PLAYERS } from '../utils/queries';
+import ShowAllPlayersForATeam from "../components/ShowAllPlayersForATeam";
 
-function Senators(props) {
-    const numbers = props.value;
-    const keys  =  props.id;
-    const fantasyPoints = props.FantasyPoints;
-    const name = props.PlayerID;
-    const position = props.Position;
-    const team = props.Team;
-    const teamID = props.TeamID;
-    
-    const {loading, data  } = useQuery(QUERY_PLAYERS);
-    const players = data?.players ||[];
-    
-    if(loading)
-    {
-        return <div>Loading...</div>;
-    }
+function Coyotes(props) {
     
   return (
-   
     <>
-        <Container  key={keys.toString()} value={teamID}>
-            <div className="my-2">
-                <div className ="player-container">
-                    <div className="headerPlayer">
-                        <div className="inheaderPlayer">
-                            <div className = "logoPlayer">
-                                <h1>Player Card</h1>
-                                    <ul className="navigationPlayer">
-                                        <li>TeamID:{players.TeamID}</li>
-                                        <li>PlayerID:{players.PlayerID}</li>
-                                        <li>Name:{players.Name}</li>
-                                        <li>Team:{players.Team}</li>
-                                        <li>FantasyPoints:{players.FantasyPoints}</li>
-                                    </ul>
-                            </div>
-                        </div>    
-                    </div>
-                </div>
-            </div>
-        </Container>
+        <ShowAllPlayersForATeam></ShowAllPlayersForATeam>
     </> 
   );
 }
 
-export default Senators;
+export default Coyotes;
